0c2c86e6b7d0f81e9ba2ea99013f5d3580f2f7f3
FebriAris01 <febriaris679@gmail.com>
2022-10-05 23:07:01 +0700

    Menambahkan file tugas 6 php

---
+++ Tugas6_PHP_FebriArismawan_UniversitasMuriaKudus_PakReza/index.php
@@ -0,0 +1,340 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>My Profile</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+	<div id="colorlib-page">
+		<div class="container-wrap">
+		<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
+		<aside id="colorlib-aside" role="complementary" class="border js-fullheight">
+			<div class="text-center">
+			<a href="index.php"><div class="author-img" style="background-image: url(images/foto.jpg);"></div></a>
+			</div>
+			<nav id="colorlib-main-menu" role="navigation" class="navbar">
+				<div id="navbar" class="collapse">
+					<ul>
+						<li class="active"><strong><a href="#" data-nav-section="home">Home</a></strong></li>
+						<li><strong><a href="#" data-nav-section="about">Tentang Saya</a></strong></li>
+						<li><strong><a href="#" data-nav-section="experience">Pendidikan</a></strong></li>
+						<li><strong><a href="#" data-nav-section="work">Tasks</a></strong></li>
+						<li><strong><a href="#" data-nav-section="blog">Portofolio</a></strong></li>
+					</ul>
+				</div>
+			</nav>
+
+			<div class="colorlib-footer">
+				<p><small>&copy; <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+			<script>document.write(new Date().getFullYear());</script> <i class="icon-heart" aria-hidden="true"></i> by <a>Febri Arismawan</a>
+			</div>
+
+		</aside>
+
+		<div id="colorlib-main">
+			<section id="colorlib-hero" class="js-fullheight" data-section="home">
+				<div class="flexslider js-fullheight">
+					<ul class="slides">
+				   	<li style="background-image: url(images/saya.jpg);">
+				   		<div class="overlay"></div>
+				   		<div class="container-fluid">
+				   			<div class="row">
+					   			<div class="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+					   				<div class="slider-text-inner js-fullheight">
+					   					<div class="desc">
+						   					<h3><font color ="white">Selamat Datang di,</h3></font>
+											   <h3><font color ="white">Halaman Web Profile Saya</h3></font>
+					   				</div>
+					   			</div>
+					   		</div>
+				   		</div>
+				   	</li>
+				  	</ul>
+			  	</div>
+			</section>
+
+			<section class="colorlib-about" data-section="about">
+				<div class="colorlib-narrow-content">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+								<div class="col-md-12">
+									<div class="about-desc">
+										<span class="heading-meta">Tentang Saya</span>
+										<h2 class="colorlib-heading">Tentang Saya</h2>
+										<p><strong>Assalamu'alaikum wr wb</strong></p>
+										<p><strong>Perkenalkan, nama saya Febri Arismawan. Sekarang, saya adalah mahasiswa semester 7 Program Studi Sistem Informasi, Fakultas Teknik di Universitas Muria Kudus. 
+											Saya adalah anak kedua dari 2 bersaudara.</p><strong>
+										<p><strong>Saya tertarik dengan dunia IT sejak saya SMK. Saya menyukai IT karena terlihat menarik dan keren untuk dipelajarai.
+											Saya menyukai pemrograman web dan mobile, walaupun sering merasa kesulitan dan stress saat sedang ngoding karena error.
+											Tapi, di situ lah membuat rasa penasaran menjadi muncul untuk memperbaiki dan menurut saya itulah hal yang menarik dari pemrograman.</p><strong>
+										<p><strong>Wassalamu'alaikum wr wb</strong></p>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-3 animate-box" data-animate-effect="fadeInLeft">
+									
+								</div>
+								<div class="col-md-3 animate-box" data-animate-effect="fadeInRight">
+									
+								</div>
+								<div class="col-md-3 animate-box" data-animate-effect="fadeInTop">
+									
+								</div>
+								<div class="col-md-3 animate-box" data-animate-effect="fadeInBottom">
+									
+								</div>
+							</div>
+							<div class="row">
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="colorlib-experience" data-section="experience">
+				<div class="colorlib-narrow-content">
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+							<span class="heading-meta">Pendidikan</span>
+							<h2 class="colorlib-heading animate-box">Pendidikan</h2>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+				         <div class="timeline-centered">
+					         <article class="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+					            <div class="timeline-entry-inner">
+
+					               <div class="timeline-icon color-1">
+					                  <i class="icon-pen2"></i>
+					               </div>
+
+					               <div class="timeline-label">
+					                  <h2>SD 7 Tanjungrejo Kudus<span> (2007 - 2013)</span></h2>
+					               </div>
+					            </div>
+					         </article>
+
+
+					         <article class="timeline-entry animate-box" data-animate-effect="fadeInRight">
+					            <div class="timeline-entry-inner">
+					               <div class="timeline-icon color-2">
+					                  <i class="icon-pen2"></i>
+					               </div>
+					               <div class="timeline-label">
+					               	<h2>SMP 2 Jekulo Kudus <span>(2013 - 2016)</span></h2>
+					               </div>
+					            </div>
+					         </article>
+
+					         <article class="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+					            <div class="timeline-entry-inner">
+					               <div class="timeline-icon color-3">
+					                  <i class="icon-pen2"></i>
+					               </div>
+					               <div class="timeline-label">
+					               	<h2>SMK PGRI 1 Mejobo Kudus - Rekayasa Perangkat Lunak <span>(2016 - 2019)</span></h2>
+					               </div>
+					            </div>
+					         </article>
+
+					         <article class="timeline-entry animate-box" data-animate-effect="fadeInTop">
+					            <div class="timeline-entry-inner">
+					               <div class="timeline-icon color-4">
+					                  <i class="icon-pen2"></i>
+					               </div>
+					               <div class="timeline-label">
+					               	<h2>Universitas Muria Kudus - Sistem Informasi <span>(2019 - sekarang)</span></h2>
+					               </div>
+					            </div>
+					         </article>
+
+					         <article class="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+					            <div class="timeline-entry-inner">
+					               <div class="timeline-icon color-none">
+					               </div>
+					            </div>
+					         </article>
+					      </div>
+					   </div>
+				   </div>
+				</div>
+			</section>
+
+			<section class="colorlib-work" data-section="work">
+				<div class="colorlib-narrow-content">
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+							<span class="heading-meta">Tasks</span>
+							<h2 class="colorlib-heading animate-box">Kumpulan Tugas</h2>
+						</div>
+					</div>
+					<div class="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+						<div class="col-md-12">
+							<p class="work-menu"><span><a>HTML</a></span> <span><a>CSS</a></span> <span><a>JAVASCRIPT</a></span> <span><a>PHP</a></span></p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6 animate-box" data-animate-effect="fadeInLeft">
+							<div class="project" style="background-image: url(images/html.png);">
+								<div class="desc">
+									<div class="con">
+										<h3><a>Tugas HTML</a></h3>
+										<p class="icon">
+											<span><a href="https://github.com/FebriAris01/tugas-html-css.git" target="_blank"><i class="icon-file"></i> File Tugas</a></span>
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 animate-box" data-animate-effect="fadeInRight">
+							<div class="project" style="background-image: url(images/css.png);">
+								<div class="desc">
+									<div class="con">
+										<h3><a>Tugas CSS</a></h3>
+										<p class="icon">
+											<span><a href="https://github.com/FebriAris01/tugas-html-css.git" target="_blank"><i class="icon-file"></i> File Tugas</a></span>
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 animate-box" data-animate-effect="fadeInTop">
+							<div class="project" style="background-image: url(images/javascript.jpg);">
+								<div class="desc">
+									<div class="con">
+										<h3><a>Tugas Javascript</a></h3>
+										<p class="icon">
+											<span><a href="https://github.com/FebriAris01/javascript.git" target="_blank"><i class="icon-file"></i> File Tugas</a></span>
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 animate-box" data-animate-effect="fadeInBottom">
+							<div class="project" style="background-image: url(images/PHP.png);">
+								<div class="desc">
+									<div class="con">
+										<h3><a>Tugas PHP</a></h3>
+										<p class="icon">
+											<span><a href="https://github.com/FebriAris01/php.git" target="_blank"><i class="icon-file"></i> File Tugas</a></span>
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="colorlib-blog" data-section="blog">
+				<div class="colorlib-narrow-content">
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+							<span class="heading-meta">Portfolio</span>
+							<h2 class="colorlib-heading">Portfolio</h2>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+							<div class="blog-entry">
+								<a class="blog-img"><img src="images/inventaris.png" class="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com"></a>
+								<div class="desc">
+									<h3>Sistem Informasi Inventaris Sarana & Prasarana Sekolah Berbasis Web</h3>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInRight">
+							<div class="blog-entry">
+								<a class="blog-img"><img src="images/catatan.jpeg" class="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com"></a>
+								<div class="desc">
+									<h3><a href="blog.html">Aplikasi Catatan Berbasis Mobile</a></h3>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+							<div class="blog-entry">
+								<a class="blog-img"><img src="images/toko hp.png" class="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com"></a>
+								<div class="desc">
+									<h3>Sistem Informasi Penjualan HP Berbasis Web</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		</div><!-- end:colorlib-main -->
+	</div><!-- end:container-wrap -->
+	</div><!-- end:colorlib-page -->
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	
+	
+	<!-- MAIN JS -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
